e109d3dcef013e2fe13dc139f5a65c295fb84e31
eithery <michael.protsenko@gmail.com>
2020-02-16 21:07:35 -0500

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer fixed app />
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Blue Trolley'
+    }
+  }
+}
+</script>
